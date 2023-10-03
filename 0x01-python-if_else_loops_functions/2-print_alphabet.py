8e8d80e14574d834145bb4ff64d00814d6f81db0
Quadr <103822802+Quadr001@users.noreply.github.com>
2023-10-03 19:31:37 +0100

    print_alphabet

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-# 2-print_alphabet.py
 
-"""Print the lowercase alphabet without a newline character"""
 for letter in range(97, 123):
     print("{}".format(chr(letter)), end="")
