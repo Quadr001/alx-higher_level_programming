fc9f50572f8d0a0a8c4171078a78d40fde6a46d7
Quadr <103822802+Quadr001@users.noreply.github.com>
2023-10-03 13:08:40 +0100

    print_alphabt

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for m in range(97, 123):
-    if m != 100 and m != 109:
-        print("{}".format(chr(m)), end="")
+for letter in range(97, 123):
+    if chr(letter) is not 'q' and chr(letter) is not 'e':
+        print("{}".format(chr(letter)), end="")
