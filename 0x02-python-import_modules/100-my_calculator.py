4d2ab33452a24f519b38187c9f32bbd343600304
Quadr <103822802+Quadr001@users.noreply.github.com>
2023-10-05 17:41:03 +0100

    my_calculator

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -4,7 +4,7 @@ if __name__ == "__main__":
     from calculator_1 import add, sub, mul, div
     argc = len(argv)
     if argc != 4:
-        print('Usage: {} <m> <operator> <n>'.format(argv[0]))
+        print('Usage: {} <a> <operator> <b>'.format(argv[0]))
         exit(1)
     ops = {
         '+': add,
@@ -13,11 +13,11 @@ if __name__ == "__main__":
         '/': div
     }
     if argv[2] in ops:
-        m = int(argv[1])
-        n = int(argv[3])
+        a = int(argv[1])
+        b = int(argv[3])
         op = ops[argv[2]]
-        result = op(num1, num2)
-        print('{:d} {:s} {:d} = {:d}'.format(m, argv[2], n, result))
+        result = op(a, b)
+        print('{:d} {:s} {:d} = {:d}'.format(a, argv[2], b, result))
     else:
         print('Unknown operator. Available operators: +, -, * and /')
         exit(1)
