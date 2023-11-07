9a5d8e08f517d1e7363a901fd5160479313dc554
Quadr <103822802+Quadr001@users.noreply.github.com>
2023-11-07 18:47:45 +0100

    class_to_json

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""function that returns the dictionary
+description with a simple data structure
+"""
+
+
+def class_to_json(obj):
+    """module class_to_json
+       returns builds a dictionary
+    """
+    return obj.
